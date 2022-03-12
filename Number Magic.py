82805ebb7945d2b43d041731900943d76508d83a
CoderguySuryansh <98791574+CoderguySuryansh@users.noreply.github.com>
2022-03-12 18:13:46 +0530

    Add files via upload

---
+++ Number Magic.py
@@ -0,0 +1,16 @@
+#TODAY WE WILL PLAY MIND GAME
+print("Choose number in your mind between 1 and 10")
+print("\n OK I think you had selected your number")
+p=input("\t\t:")
+print(" Now double your number ")
+q=input("\t\t:")
+print(" Now add 6 to your number ")
+r=input("\t\t:")
+print(" Now divide number by 2 ")
+s=input("\t\t:")
+print("Take away the number you have thought from the answer, that is, subtract the answer from the number you have thought")
+print(":")
+a=3
+print("THE NUMBER NOW IN YOUR MIND :" ,a)
+
+
